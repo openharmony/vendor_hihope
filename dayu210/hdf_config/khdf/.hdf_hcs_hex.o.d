751739ef3bc63395ac292ff0728042bf32aeb688
ma_yulong <ma_yulong@hoperun.com>
2024-03-15 09:47:52 +0800

    deleted useless file hdf_hcs_hex.o.d
    Signed-off-by: ma_yulong <ma_yulong@hoperun.com>

--- dayu210/hdf_config/khdf/.hdf_hcs_hex.o.d
+++
@@ -1,5 +0,0 @@
-drivers/hdf/khdf/../../../../../vendor/hihope/dayu210/hdf_config/khdf/hdf_hcs_hex.o: \
- ../../../../vendor/hihope/dayu210/hdf_config/khdf/hdf_hcs_hex.c \
- include/linux/kconfig.h include/generated/autoconf.h \
- include/linux/compiler_types.h include/linux/compiler_attributes.h \
- include/linux/compiler-gcc.h arch/arm64/include/asm/compiler.h
