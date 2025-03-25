b46f900a5a032b522437dadb39a0eb400be948ea
stone-shi1 <shilei@hoperun.com>
2025-03-25 15:55:25 +0800

    16_sle_gas_02
    Signed-off-by: stone-shi1 <shilei@hoperun.com>

--- nearlink_dk_3863/ws63_sample/26_sle_gas/adc_driver/替换3/adc/v154/hal_adc_v154_regs_op.c
+++ nearlink_dk_3863/ws63_sample/26_sle_gas/adc_driver/替换3/adc/v154/hal_adc_v154_regs_op.c
@@ -1,10 +1,20 @@
 /**
- * Copyright (c) HiSilicon (Shanghai) Technologies Co., Ltd. 2024-2024. All rights reserved.
+ * Copyright (c) 2020 HiSilicon (Shanghai) Technologies CO., LIMITED.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Description: Provides V154 adc register operation api \n
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  *
+ * Description: Provides adc port \n
  *
  * History: \n
- * 2024-02-01， Create file. \n
+ * 2022-09-16， Create file. \n
  */
 
 #include "common_def.h"
