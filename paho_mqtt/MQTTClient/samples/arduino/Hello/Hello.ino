4329405ad35017d298ae5a7f67e2973243683444
stone.shi <shilei@hoperun.com>
2025-03-31 18:26:42 +0800

    paho_mqtt_01
    Signed-off-by: stone.shi <shilei@hoperun.com>

--- paho_mqtt/MQTTClient/samples/arduino/Hello/Hello.ino
+++ paho_mqtt/MQTTClient/samples/arduino/Hello/Hello.ino
@@ -1,19 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2014 IBM Corp. and others
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v1.0 which accompany this distribution. 
- *
- * The Eclipse Public License is available at 
- *   http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
- *   http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- *    Ian Craggs - initial contribution
- *    Benjamin Cabe - adapt to IPStack, and add Yun instructions
- *******************************************************************************/
+# Copyright (c) IBM Corp.
+# Licensed under the Apache License, Version 2.0 (theLicense");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-20
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+*******************************************************************************/
 
 #define MQTTCLIENT_QOS2 1
 
