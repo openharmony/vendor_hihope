bb40efe7180a8d683b2dee5057c8830d8ef5acf2
stone.shi <shilei@hoperun.com>
2025-03-31 18:34:48 +0800

    paho_mqtt_01
    Signed-off-by: stone.shi <shilei@hoperun.com>

--- paho_mqtt/MQTTClient/samples/arduino/Hello/Hello.ino
+++ paho_mqtt/MQTTClient/samples/arduino/Hello/Hello.ino
@@ -1,5 +1,5 @@
 /*******************************************************************************
-# Copyright (c) IBM Corp.
+# Copyright (c) 2023 IBM Corp.
 # Licensed under the Apache License, Version 2.0 (theLicense");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
