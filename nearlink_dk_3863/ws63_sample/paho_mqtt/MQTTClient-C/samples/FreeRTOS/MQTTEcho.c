e9d8ebc7cd578a7cfb60f006836477039cc6619f
stone.shi <shilei@hoperun.com>
2025-04-01 15:57:24 +0800

    paho_mqtt_01
    Signed-off-by: stone.shi <shilei@hoperun.com>

--- nearlink_dk_3863/ws63_sample/paho_mqtt/MQTTClient-C/samples/FreeRTOS/MQTTEcho.c
+++ nearlink_dk_3863/ws63_sample/paho_mqtt/MQTTClient-C/samples/FreeRTOS/MQTTEcho.c
@@ -1,19 +1,21 @@
- /*
-#*******************************************************************************
-#  Copyright (c) 2017 IBM Corp.
-#
-#  All rights reserved. This program and the accompanying materials
-#  are made available under the terms of the Eclipse Public License v1.0
-#  and Eclipse Distribution License v1.0 which accompany this distribution.
-#
-#  The Eclipse Public License is available at
-#     http://www.eclipse.org/legal/epl-v10.html
-#  and the Eclipse Distribution License is available at
-#    http://www.eclipse.org/org/documents/edl-v10.php.
-#
-#  Contributors:
-#     Ian Craggs - initial version
-#*******************************************************************************/
+/**
+ * Copyright (c) 2020 HiSilicon (Shanghai) Technologies CO., LIMITED.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  *
+ * Description: Provides adc port \n
+ *
+ * History: \n
+ * 2022-09-16， Create file. \n
+ */
 
 /* Standard includes. */
 #include <stdint.h>
