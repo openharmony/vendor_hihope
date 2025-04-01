b60e5f6b24d1048ac8dc1a8b2d039281454555e8
stone.shi <shilei@hoperun.com>
2025-04-01 15:08:52 +0800

    paho_mqtt_01
    Signed-off-by: stone.shi <shilei@hoperun.com>

--- nearlink_dk_3863/ws63_sample/paho_mqtt/MQTTClient/samples/arduino/Hello/Hello.ino
+++ nearlink_dk_3863/ws63_sample/paho_mqtt/MQTTClient/samples/arduino/Hello/Hello.ino
@@ -1,5 +1,5 @@
-/**
- * Copyright (c) 2020 IBM Corp., LIMITED.
+ /**
+ * Copyright (c) 2020 IBM., LIMITED.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,10 +11,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.  *
- * Description: Provides adc port \n
- *
- * History: \n
- * 2022-09-16， Create file. \n
  */
 
 #define MQTTCLIENT_QOS2 1
